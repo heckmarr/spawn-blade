7b17541a23cbfbd90a7ea8eb661758d089d60933
Ent <rainbow.punk.treant@gmail.com>
2020-12-16 17:49:44 -0800

    first iteration, second monitor setup

---
+++ spawn.sh
@@ -0,0 +1,6 @@
+#!/bin/fish
+/usr/bin/codium & 
+terminator -T 'Local Sync' --geometry=700x350+3122+700 --profile=candy-fluff  & 
+terminator -T 'Local Build' --geometry=600x350+1922+700  --profile=candy-fluff & 
+terminator -T 'Remote Root' --geometry=300x350+2521+700 --profile=oo & 
+terminator  -T 'Remote Unpriviledged' --geometry=300x350+2822+700 --profile=oo &
