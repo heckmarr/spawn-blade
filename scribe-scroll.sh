75c67b6ecb5ad44c1546384e9ec64cc679fbe250
Entropy_ <benbelland@gmail.com>
2021-02-06 00:30:44 -0800

    updates the profile names

--- scribe-scroll.sh
+++ scribe-scroll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo cp -v ~/Code/spawn-blade/summon.sh /usr/bin/summon-treant
 sudo cp -v ~/Code/spawn-blade/summon-3.sh /usr/bin/summon-dryad
-sudo cp -v ~/Code/spawn-blade/summon4 /usr/bin/compact
+sudo cp -v ~/Code/spawn-blade/summon4.sh /usr/bin/compact
 exit
