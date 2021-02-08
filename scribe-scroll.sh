7b982e563e42484c917da85f42c5d1e918907b07
Entropy <benbelland@gmail.com>
2021-02-08 07:51:13 -0800

    save point

--- scribe-scroll.sh
+++ scribe-scroll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -v ~/Code/spawn-blade/summon.sh ~/bin/summon-treant
-cp -v ~/Code/spawn-blade/summon-3.sh ~/bin/summon-dryad
-cp -v ~/Code/spawn-blade/compact.sh ~/bin/compact
+sudo cp -v /code/spawn-blade/summon.sh ~/bin/summon-treant
+sudo cp -v /code/spawn-blade/summon-3.sh ~/bin/summon-dryad
+sudo cp -v /code/spawn-blade/compact.sh ~/bin/compact
 exit
