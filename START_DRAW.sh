33e2f94049df1a7c9cd81f3ea4d9aad175c4cf6e
Ben Belland <benbelland@gmail.com>
2024-03-01 18:47:54 -0800

    updated the script to specify which hdmi it should use
    in general because I'm on wayland I have to run the script
    in the screen i want to effect

--- START_DRAW.sh
+++ START_DRAW.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 rm .tmp_start_draw
 id="id=";
-xinput | grep "Pen Pen" >> .tmp_start_draw
+xinput | grep "stylus" >> .tmp_start_draw
 while read p; do
 	LAST=${p##*id=};
 	NUM=`echo $LAST | awk '{print $1}'`
 	echo 'Device is '$NUM;
 done <.tmp_start_draw
-xinput map-to-output $NUM HDMI-0;
-echo 'OUTPUT MAPPED TO HDMI-0';
+xrandr | grep "HDMI" > .tmp_start_draw;
+while read p; do
+	DEV=`echo $p | awk '{print $1}'`
+	echo 'Display is '$DEV;
+done <.tmp_start_draw
+
+xinput map-to-output $NUM $DEV;
+echo 'OUTPUT '$NUM' MAPPED TO '$DEV;
